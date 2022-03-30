7fee04a695f0d375b30dce3c6375688ffdafb242
din974 <luidgi.parinet@epitech.eu>
2022-03-30 11:58:28 +0400

    push

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-export default function App() {
+export  default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: "blue",
+        alignItems: "center",
+        justifyContent: "center",
+    }}
+    >
+      <Text>Hello, world!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
